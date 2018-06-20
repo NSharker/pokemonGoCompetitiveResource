8935cb9ced6b2db90636a274fd326d67dd99b2e4
Nukki <nikki6052096@gmail.com>
2018-06-20 08:26:15 -0400

    show basic info of one pok

--- frontend/app/components/OnePokemon.js
+++ frontend/app/components/OnePokemon.js
@@ -2,7 +2,20 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 const OnePokemon = ({ thePokemon }) => (
-  <div> hello {thePokemon.description} </div>
+  <div className="one-pok-top">
+    <div className="one-pok-top box">
+      <img className="other-bg" src={`/public/images/${thePokemon.name}.png`} alt="logo" />
+      <div>
+        <div className="other-bg"> #{thePokemon.dex} {thePokemon.name} </div>
+        <div className="other-bg"> Max CP: {thePokemon.maxcp} </div>
+        <div className="other-bg"> Attack: {thePokemon.attack} </div>
+        <div className="other-bg"> Defense: {thePokemon.defense} </div>
+        <div className="other-bg"> Stamina: {thePokemon.stamina} </div>
+        <div className="other-bg"> Type: {thePokemon.types} </div>
+        <div className="other-bg"> Description: {thePokemon.description} </div>
+      </div>
+    </div>
+  </div>
 );
 
 const mapStateToProps = ({ thePokemon }) => ({ thePokemon });
