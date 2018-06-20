bd62b77c299d001ee0359c649e35fdd135af8f36
Nishad Sharker <nishad.sharker@gmail.com>
2018-06-20 00:23:06 -0400

    Fix plural form of Pokemon

--- backend/pokemonGoComp/config/routes.rb
+++ backend/pokemonGoComp/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace 'api' do
 	namespace 'v1' do
-		resources :pokemons, param: :dex, only: [:index, :show]
+		resources :pokemon, param: :dex, only: [:index, :show]
 	end
   end
 end
