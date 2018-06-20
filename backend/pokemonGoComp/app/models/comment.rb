26c7ef28bfef9aafb17fbe2b4af57fc3b5c4d8fb
Nishad Sharker <nishad.sharker@gmail.com>
2018-06-20 11:23:30 -0400

    Add sentiment to migration

--- backend/pokemonGoComp/app/models/comment.rb
+++ backend/pokemonGoComp/app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :pokemon
+  default_scope -> { order(created_at: :desc) }
+  validates :pokemon_id, presence:true
 end
--- backend/pokemonGoComp/db/migrate/20180620145923_create_comments.rb
+++ backend/pokemonGoComp/db/migrate/20180620145923_create_comments.rb
@@ -3,9 +3,11 @@ class CreateComments < ActiveRecord::Migration[5.1]
     create_table :comments do |t|
       t.string :author
       t.text :content
+	  t.string :sentiment
       t.references :pokemon, foreign_key: true
 
       t.timestamps
     end
+	add_index :comments, [:pokemon_id, :created_at]
   end
 end
