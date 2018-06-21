77de4bdede3e9f9569fe046573739b91308bd5e6
Nishad Sharker <nishad.sharker@gmail.com>
2018-06-21 11:53:45 -0400

    Add CORS functionality

--- backend/pokemonGoComp/config/initializers/application_controller_renderer.rb
+++ backend/pokemonGoComp/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,14 @@
 #     https: false
 #   )
 # end
+
+
+
+class Application < Rails::Application
+	 config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :options]
+      end
+    end
+end
